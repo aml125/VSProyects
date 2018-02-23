1b4bc1cd5bc2ca5c1ba0b430b90a6ef0c25760de
unknown <aml125@alu.ua.es>
2018-02-23 12:52:48 +0100

    proque

--- Burbuja/Burbuja/Burbuja.cpp
+++ Burbuja/Burbuja/Burbuja.cpp
@@ -4,40 +4,62 @@
 
 using namespace std;
 
-int burbuja(int* lista);
+void burbuja(int* lista);
+
+void mostrarLista(int* lista)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		cout << lista[i] << " ";
+	}
+	cout << endl;
+}
+
 int main( void )
 {
-	int lista []={0,1};
-	burbuja(lista);
-	cout << lista[0] << " " << lista[1];
+	int lista []={2,1,3};
+	mostrarLista(lista);
 	char fuck;
 	cin >> fuck;
 }
 
-int burbuja(int* lista)
+void burbuja(int* lista)
 {
-	int i = 0;
-	int j = 0;
-	//int n = sizeof(lista);
-	int n = 20; //Tamaño del array
 
 	__asm
 	{
-		mov esi, list; Cargar la lista
+		mov edx, lista; Cargar la lista
+		mov ebx, 0; Variable i = 0
+		mov ecx, 3; size
+		
+		mov eax, [edx + 4]
+
 bucle1:
-		cmp i, n
-		jl fin1; Hasta i = n - 1
-	bucle2:
-			mov ecx, n   ; j = n
-			mov eax, i 
-			cmp eax ecx
-			jg fin2
+		cmp ebx, ecx; bucle1
+		jge fin1; Hasta i = n - 1 (incluido)
+			mov ebp, ecx; variable j = n - 1
+			dec ebp
+bucle2:
+			cmp ebp, ebx; bucle 2
+			jle fin2; Hasta j = i + 1 (incluido)
+				mov esi, [edx + (ebp - 1)*4]; operando 1
+				mov edi, [edx + ebp*4]; operando 2
+				cmp esi, edi; si (vector[j-1]<vector[j])
+				jl swap
+endif:
+			dec ebp; j--
+			jmp bucle2
+fin2:
+		inc ebx
+		jmp bucle1
+fin1:
 
-			mov ebx, [esi]
-			mov edx, 
-
-
-		// devuelve el resultado de eax
+jmp fin
+swap:
+		mov eax, [edx + (ebp - 1)*4] ;intercambiar(vector[j-1],vector[j])
+		xchg eax, [edx + ebp*4]
+		mov [edx + (ebp - 1)*4], eax
+		jmp endif
+fin:
 	}
-	return 0;
 }
